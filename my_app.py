cb032d4038b9b8d1aa000aeff1a16e20e5519b2c
Ildar <kaiiiapa1613@gmail.com>
2024-08-09 21:31:34 +0300

    v 2.0

--- my_app.py
+++ my_app.py
@@ -6,6 +6,7 @@ from database import *
 from utils.templating import TemplateResponse
 from fastapi.responses import HTMLResponse
 from fastapi import Request, Depends
+from fastadmin import fastapi_app as admin_app
 
 
 # создаем таблицы
@@ -14,6 +15,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 # определяем зависимость
 def get_db():
@@ -48,6 +50,19 @@ def news_page(request: Request, db: Session = Depends(get_db)):
     }
     return TemplateResponse(request=request, name="news_page.html", context=context)
 
+
+@app.get(
+    "/news/{id}",
+    name="new:page_new",
+    response_class=HTMLResponse
+)
+def new_page(id, request: Request, db: Session = Depends(get_db)):
+    news = db.query(NewsFilm).filter_by(id=id).first()
+    print(news.id)
+    context = {
+        "news": news
+    }
+    return TemplateResponse(request=request, name="new_page.html", context=context)
 
 @app.get(
     "/registration/",
@@ -100,28 +115,37 @@ def registration_user_page(request: Request, username: str = Form(), password: s
         return TemplateResponse(request=request, name="registration_page.html",
                                 context={'wellcome': f'Поздравляю вы зарегистрированы, теперь можете авторизоваться'})
 
-@app.get(
-    "/login/",
-    name="login:login_page",
-    response_class=HTMLResponse
-)
-def login_page(request: Request):
-    return TemplateResponse(request=request, name="login_page.html")
+# Реализуем отдельный CRUD для модели NewsFilm
+@app.post("/NewsFilm/")
+def creat_NewsFilm(title: str, summary: str, img_url: str, description: str, db: Session = Depends(get_db)):
+    news = NewsFilm(title=title, summary=summary, description=description, img_url=img_url)
+    db.add(news)  # добавляем в бд
+    db.commit()
+    return "Новость добавлена"
 
+@app.get("/NewsFilm/")
+def read_NewsFilm(db: Session = Depends(get_db)):
+    news = db.query(NewsFilm).order_by(NewsFilm.created_at.desc()).all()
+    return news
 
-@app.post(
-    "/login/",
-    name="login:login_page",
-    response_class=HTMLResponse
-    )
-def login_page_user(request: Request, username: str = Form(), password: str = Form(), db: Session = Depends(get_db)):
-    info = {}
-    username = username
-    password = password
-    hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-    user = db.query(User).filter(User.username == username).first()
-    if user.hashed_password == hashed_password:
-        # как это сделать??
+@app.put("/NewsFilm/")
+def update_NewsFilm(id: int, title: str, summary: str, img_url: str, description: str, db: Session = Depends(get_db)):
+    news = db.query(NewsFilm).filter(NewsFilm.id == id).first()
+    if news is not None:
+        news.title = title
+        news.summary = summary
+        news.description = description
+        news.img_url = img_url
+        db.commit()  # сохраняем изменения
+        # проверяем, что изменения применены в бд - получаем один объект
+        new_news = db.query(NewsFilm).filter(NewsFilm.id == id).first()
+        return f'Новость обновлена {new_news}'
     else:
-        info['error'] = 'Неправильное имя пользователя или пароль'
-        return TemplateResponse(request=request, name="login_page.html", context=info)
+        return f'Новости с таким id {id} не существует'
+
+@app.delete("/NewsFilm/")
+def delete_NewsFilm(id: int, db: Session = Depends(get_db)):
+    news = db.query(NewsFilm).filter(NewsFilm.id == id).first()
+    db.delete(news)
+    db.commit()
+    return 'Новость удалена'
